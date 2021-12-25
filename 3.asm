3c848a9e955048e096cc27584adfbac7b5fc88b7
Quarysite <60838492+Quarysite@users.noreply.github.com>
2021-12-25 16:29:31 +0300

    Add files via upload

---
+++ 3.asm
@@ -0,0 +1,45 @@
+%include "io.inc"
+
+section .text
+global CMAIN
+CMAIN:
+    LEA ESI, [a]           
+    LEA EDX, [ESI]          
+    LEA EDI, [5+ESI]        ;N = len(a)
+MN:                         ;for i = 0 to N-1
+    CMP EDI, ESI
+    JE END                  
+    MOV EBX, ESI            ;min = i
+    MOV ECX, ESI
+    INC ECX                 ;j = i+1
+SUBMN:                      ;for j = i+1 to N
+    MOV AH, [ECX]           
+    MOV AL, [EBX]           
+    CMP EDI, ECX
+    JE COMPARISON           ;if j = N
+    CMP AH, AL      
+    JA INCJ                 ;if a[j] > a[min]
+    MOV EBX, ECX            ;min = j
+INCJ:
+    INC ECX
+    JMP SUBMN        
+COMPARISON:
+    MOV AH, [ESI]
+    CMP ESI, EBX            
+    JE INCI                 ;if i = min
+    MOV [ESI], AL           
+    MOV [EBX], AH           ;swap(a[i], a[min])
+INCI:
+    INC ESI
+    JMP MN    
+END:
+    PRINT_DEC 1, [EDX]
+    NEWLINE
+    INC EDX
+    CMP EDI, EDX
+    JA END
+    RET 
+
+    
+section .data
+a: db 31, 14, 56, 49, 9
